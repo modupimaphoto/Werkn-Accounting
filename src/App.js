a7b1633e078bf1a191617b216d9f7bdcbb9787a4
modupi <modupimaphoto@gmail.com>
2023-06-11 11:19:29 +0200

    react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+const App = () => {
+	
+	return(
+		<>
+			<h1 className='text-center'>
+				Werkn react app.
+			</h1>
+			<p className='text-center'>
+				I come from <code>App.js</code> file.
+			</p>
+		</>
+	);
+}
+
+export default App;
