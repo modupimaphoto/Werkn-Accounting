d40bd3b984d40fac718211ad0d691387bf8be4b0
modupi <modupimaphoto@gmail.com>
2023-06-12 20:19:52 +0200

    our team section file

---
+++ src/ourTeam.js
@@ -0,0 +1,15 @@
+
+const OurTeam = () => {
+
+    return(
+        <div className='row'>
+            <div className='col-md-6'>
+                <h3>Our Team.</h3>
+            </div>
+            <div className='col-md-6'>
+                
+            </div>
+        </div>
+    )
+}
+export default OurTeam;
