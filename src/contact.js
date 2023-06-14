8e157e21e501e26c2412be0d1d7c5296e7b97bad
modupi <modupimaphoto@gmail.com>
2023-06-14 11:30:37 +0200

    prevent form default

--- src/contact.js
+++ src/contact.js
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 
 const Contact = () => {
-    const handleSubmit = () => {
-
+    const handleSubmit = (event) => {
+        event.preventDefault();
     }
     return(
         <>
             <div className='form-wrapper  p-3'>
-                <h3 className='text-EG'>Contact us.</h3>
-                <p className='width-50'>
+                <h3 className='text-C'>Contact us.</h3>
+                <p className='width-50 text-platinum'>
                     We would love to hear from you. 
                     Whether you have a question, feedback, or need our services, 
                     please feel free to <span className='text-C'>contact</span> us .
@@ -16,17 +16,17 @@ const Contact = () => {
                 <div className='row g-4'>
                     <div className='col-md-8  width-50'>
                         <form onSubmit={ handleSubmit }>
-                            <div className='form-group mb-4'>
+                            <div className='form-group mb-4 text-platinum'>
                                 <label for='name'>Name</label>
                                 <input type='text' placeholder="John Doe"/>
                                 <span className="error"></span>
                             </div>
-                            <div className='form-group mb-4'>
+                            <div className='form-group mb-4 text-platinum'>
                                 <label for='email'>Email</label>
                                 <input type='text' placeholder="johndoe@gmail.com"/>
                                 <span className="error"></span>
                             </div>
-                            <div className='form-group mb-4'>
+                            <div className='form-group mb-4 text-platinum'>
                                 <label for='message'>Your message</label>
                                 <textarea col='5' placeholder='Type your message here...'></textarea>
                                 <span className="error"></span>
@@ -37,24 +37,24 @@ const Contact = () => {
                         </form>
                     </div>
                     <div className='col-md-4'>
-                        <h3 className='text-EG'>
+                        <h3 className='text-C'>
                             Get in touch with us.
                         </h3>
                         <ul className='p-0'>
-                            <li className='list-group-item mb-3'>
+                            <li className='list-group-item mb-3 text-platinum'>
                                 <span className='text-C h6'>Contact centre operation hours:</span> 08:00 to 16:00 Monday to Friday<br />
                                 Closed on weekends and public holidays
                             </li>
-                            <li className='list-group-item mb-3'>
-                                <span className='text-C h6'>For queries</span> help@werkn.co.za
+                            <li className='list-group-item mb-3 text-platinum'>
+                                <span className='text-C h6'>For queries:</span> help@werkn.co.za
                             </li>
-                            <li className='list-group-item mb-3'>
+                            <li className='list-group-item mb-3 text-platinum'>
                                 <span className='text-C h6'>Phone:</span> +27 11 234 5678
                             </li>
-                            <li className='list-group-item mb-3'>
+                            <li className='list-group-item mb-3 text-platinum'>
                                 <span className='text-C h6'>Email:</span> info@werkn.co.za
                             </li>
-                            <li className='list-group-item mb-3'>
+                            <li className='list-group-item mb-3 text-platinum'>
                                 <span className='text-C h6'>Address</span> 123 Main Street, Johannesburg, Gauteng, 2000
                             </li>
                         </ul>
